bf498a4d49475be897a6d27cbb77e51faa1387ee
KevinSonda <kevinsonda@gmail.com>
2024-07-12 10:09:06 +0200

    [UPD]aggiungo la classe square al div

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@
 function createSingleSquare(num) {
     // creao il quadrato come div
     const square = document.createElement('div');
+
+    // aggiungo la classe square al div
+    square.innerText = num + 1;
 }
